5fcbc33df2ed36771d626dbca3b9ea4bbb80c069
mohamadreza <mohamadrezakhf@gmail.com>
2025-02-02 21:28:48 +0330

    customer logger init

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+// import { Logger } from '@nestjs/common';
+// import { CustomLogger } from './config/custom.logger';
+import { ValidationPipe } from '@nestjs/common';
+import { CustomLogger } from './config/custom.logger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  const customlogger = new CustomLogger('Bootstrap');
+
+  await app.listen(3000);
+  customlogger.debug(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
